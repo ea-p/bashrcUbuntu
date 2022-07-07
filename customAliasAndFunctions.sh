2bb40a21cdb23291d807ff22deccfbc28cfc43c9
ea-p <e.a.priala@gmail.com>
2022-07-07 13:19:31 +0300

    Added git alias.

--- customAliasAndFunctions.sh
+++ customAliasAndFunctions.sh
@@ -3,35 +3,35 @@
 # Aliases
 #########
 
-#Copy with progress bar
+# Copy with progress bar
 alias cpv='rsync -ah --info=progress2'
 
-#cd options
+# cd options
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
 
-#Create subfolders too
+# Create subfolders too
 alias mkdir='mkdir -pv'
 
-#Alias sudo
+# Alias sudo
 alias sudo='sudo '
 
-#install now aliases
+# install now aliases
 alias install='apt-get install -y '
 alias update='apt-get upgrade -y '
 alias search='apt-cache search '
 
-#List all
+# List all
 alias ll='ls -lah --color=auto'
 
-#List in columns
+# List in columns
 alias ls='ls -CF'
 
-#View mounted voumes
+# View mounted voumes
 alias mnt="mount | awk -F' ' '{ printf \"%s\t%s\n\",\$1,\$3; }' | column -t | egrep ^/dev/ | sort"
 
-#Return to previous folder
+# Return to previous folder
 alias back='cd "$OLDPWD"'
 
 ## Colorize the grep command output for ease of use (good for log files)##
@@ -39,13 +39,26 @@ alias grep='grep --color=auto'
 alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 
-#Untar files
+# Untar files
 alias untar='tar -zxvf '
 
-#Clear
+# Clear
 alias c='clear'
 
-#cd into $HOME
+# handy short cuts #
+alias h='history'
+alias j='jobs -l'
+alias gh='history|grep'
+
+# Git
+
+gitc() {
+    git add .
+    git commit -m "$1"
+    git push
+}
+
+# cd into $HOME
 
 cd "$HOME"
 
